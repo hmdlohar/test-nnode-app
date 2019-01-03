7be051ae3b0c161113df80452ed608456418c74e
hamid <hmdlohar@gmail.com>
2019-01-03 06:45:22 +0530

    added package json and some other things

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('static'));
+
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/home', (req, res) => res.sendFile( __dirname + "/home.html"))
 
 app.get('/about',function(req, res){
 
