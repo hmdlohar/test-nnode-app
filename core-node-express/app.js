759bf10a5c0efb521173a5227ebafb9d866ebb65
hamid <hmdlohar@gmail.com>
2019-01-15 10:58:37 +0530

    testss

--- core-node-express/app.js
+++ core-node-express/app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 2999
+const MongoClient = require("mongodb").MongoClient
+
+var db;
+
+
+MongoClient.connect('mongodb://localhost:27017', (err, database) => {
+  	console.log("db is connected");
+  	db = database.db("student_db");
+  	
+})
+
+
 
 app.use(express.static('plugins'));
 app.use(express.static('js'));
@@ -19,6 +31,41 @@ app.get('/about',function(req, res){
 	res.send("this is about file. mod");
 	
 });
+
+app.get('/api/books', (req, res) => {
+    res.json([
+            {
+                id: 1,
+                title: "Alice's Adventures in Wonderland",
+                author: "Charles Lutwidge Dodgson"
+            },
+            {
+                id: 2,
+                title: "Einsteins Dreams",
+                author: "Alan Lightman"
+            }
+        ])
+})
+
+app.get('/insertOneStudent',(req,res)=>{
+	
+	res.send("hello");
+	return;
+	var lastRollNo = db.collection("students").find().limit(1).sort({$natural:-1}).toArray((err,arr) => {
+		console.log(req.query)
+		db.collection("students").insertOne(req.query,(err,status)=>{
+			if(err)
+				console.log("err:" ,err.errmsg)
+			if(status)
+				res.json(status);
+			else
+				res.send("error occured");
+		});
+		
+	})
+	
+	
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ core-node-express/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class SimpleApp extends React.Component{
+    render(){
+        return (
+            <h1>This is whole app</h1>
+        )
+    }
+}
